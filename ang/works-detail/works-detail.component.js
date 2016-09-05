b6d5e1cbe576528496a58658d474b35183ed7f75
JackMasterson <jackmasterson5@gmail.com>
2016-09-05 07:40:27 -0400

    add all details jsons

--- ang/works-detail/works-detail.component.js
+++ ang/works-detail/works-detail.component.js
@@ -4,11 +4,11 @@ angular.
 	module('worksDetail').
 	component('worksDetail', {
 		templateUrl: 'works-detail/works-detail.template.html',
-		controller: ['$http', 
-			function WorksDetailController($http){
+		controller: ['$http', '$routeParams', 
+			function WorksDetailController($http, $routeParams){
 				var self = this;
 
-				$http.get('works/works.json').then(function(response){
+				$http.get('details/' + $routeParams.id + '.json').then(function(response){
 					self.details = response.data;
 					console.log(response.data);
 				})
